a996686f3a7684f53d32865f1289ddab9cb76758
Mithun Ram Kumar <mithun.arumugam@rabobank.nl>
2025-12-23 01:47:11 +0100

    Fixed compilation issue

--- demo/src/main/java/com/mithun/kafka/demo/repository/UserReactiveRepository.java
+++ demo/src/main/java/com/mithun/kafka/demo/repository/UserReactiveRepository.java
@@ -2,6 +2,7 @@ package com.mithun.kafka.demo.repository;
 
 import com.mithun.kafka.demo.mongo.UserDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface UserReactiveRepository extends MongoRepository<UserDocument, String> {
+public interface UserReactiveRepository extends ReactiveMongoRepository<UserDocument, String> {
 }
